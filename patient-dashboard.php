e4cdad68dcd265ea8450b7657ba8720a408b6368
aysha9801 <146845992+aysha9801@users.noreply.github.com>
2023-12-01 17:06:09 +0600

    Add files via upload

--- patient-dashboard.php
+++ patient-dashboard.php
@@ -43,6 +43,10 @@
         </div>
     </header>
 
+    <section>
+
+    </section>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
