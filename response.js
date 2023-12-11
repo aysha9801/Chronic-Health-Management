839b62661ae37c6aafe9bd4fd44008c0cd4f7349
Taslima Akter <taslimatasu2550@gmail.com>
2023-12-11 13:41:09 +0600

    2nd

--- response.js
+++ response.js
@@ -2,11 +2,16 @@ const responseObj = {
     hello: "Hey ! How are you doing ?",
     hey: "Hey! What's Up",
     date: new Date().toDateString(),
-    time: new Date().toLocaleTimeString(),
     fever:"Paracetamol",
     cold:"acetaminophen or ibuprofen",
     diabetes:"Insuline, Comete",
     als:"Riluzole and edaravone",
     arthritis:"Biologics, Analgesics",
+    week: "milk, calcium, fruits",
+    chickenpox: "Water, Juice",
+    Asthma: "Albuterol (ProAir HFA, Proventil HFA, Ventolin HFA)",
+    Cancer: "This is sometimes called tapering. Going slowly down to the dose you want to get to will give your mind and your body time to adjust to being without it. It's important to do it gradually. Whenever I've suddenly stopped taking my medication, I end up feeling horrible and just back at square one.",
+
+    
 
 }
